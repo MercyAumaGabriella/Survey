077632fd3a86b2cbe240d84607c182264768153a
Mercy Auma <aumagabriella@gmail.com>
2021-09-21 12:10:54 +0300

    First commit

---
+++ class.js
@@ -0,0 +1,7 @@
+function tech_care (name, price, color){
+    this.name = name;
+    this.price = price;
+    this.color = color;
+}
+
+let tech_care1 = new tech_care('Mercy', 'unpricable', 'Orange');
